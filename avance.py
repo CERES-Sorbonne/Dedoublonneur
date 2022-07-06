c699f4b74a323b849460adf51489acbfdfc77fe4
Marceau-h <106751184+Marceau-h@users.noreply.github.com>
2022-07-06 11:45:39 +0200

    Allows to recover the duplicates indices.
    For instance, you can recover this list to drop items of a dictionnary/json.
    
    You also can recover the processed list of duplicates and the cleaned one.

--- avance.py
+++ avance.py
@@ -10,40 +10,45 @@ from sklearn.metrics.pairwise import cosine_distances as pairwise_cos_dist
 
 class avance():
     
-    def __init__(self, liste_txt : List[str], taillegroupe : int = 1000, NB_PIVOTS : int = 50) -> None:
+    def __init__(self, liste_txt : list[str], NB_PIVOTS : int = 50) -> None:
         self.liste_txt = liste_txt
-        self.taillegroupe = taillegroupe
         self.NB_PIVOTS = NB_PIVOTS
         
         tqdm.write("\nDédoublonage avancé")
         
         with tqdm(total=2, desc = "Calcul des vecteurs BoW") as pbar:
-            avg = np.mean([len(e) for e in liste_txt])
-            docspivots = sample(liste_txt, NB_PIVOTS)
-
+            docspivots = sample(list(enumerate(liste_txt)), NB_PIVOTS)
+            textespivots = {e for _, e in docspivots}
             #On crée les vecteurs pour les textes (coordonnée n = occurences du mot de position n dans la liste de vocabulaire)
-            vocab = set(' '.join(docspivots).lower().split())
+            vocab = set(' '.join(textespivots).lower().split())
             vectorizer = TfidfVectorizer(stop_words = ("english"), vocabulary = vocab)
             pbar.update()
             tf = vectorizer.fit_transform(liste_txt)
             pbar.update()
-            arrtf = tf.toarray()
+            self.arrtf = tf.toarray()
+            self.pivots = docspivots
 
-            #On sépare les textes en groupes de même taille, en fonction de leur longueur
-            liste_txt = sorted(liste_txt, key=lambda liste_txt: len(liste_txt))
-            nbgroupes = 1 + (len(liste_txt) // taillegroupe)
-            groupes = [liste_txt[i*taillegroupe:(i+1)*taillegroupe] for i in range(nbgroupes)]
 
-        colors = list(Color("blue").range_to(Color("green"),nbgroupes))
+    def process(self, taillegroupe : int = 1000, sensibilite : float = 0.00001) -> None:
+        index_doublons , liste_doublons= [], []
+        liste_txt = self.liste_txt
+        self.taillegroupe = taillegroupe
+        avg = np.mean([len(e) for e in liste_txt])
+        tupliste = [(txt, i) for i, txt in enumerate(liste_txt)]
+        tupliste = sorted(tupliste, key=lambda tupliste: len(tupliste))
+
+        #On sépare les textes en groupes de même taille, en fonction de leur longueur    
+        nbgroupes = 1 + (len(tupliste) // taillegroupe)
+        groupes = [tupliste[i*taillegroupe:(i+1)*taillegroupe] for i in range(nbgroupes)]
+
+        colors = [Color("blue")] +list(Color("blue").range_to(Color("green"),nbgroupes))
         
         manualpbar = tqdm(groupes, desc="Calcul des vecteurs seconds / supression")
-        doublons_tot = {}
-        cquoilesdoublons = {}
         for i, groupe in enumerate(manualpbar):
             manualpbar.colour = colors[i].hex_l
             #Array de tous les vecteurs 2, un texte par ligne comparé a chaque pivot par colonne
-            vect1_pivot = np.array([arrtf[i] * (avg / len(e["texte"])) for i, e in enumerate(docspivots)], dtype = np.half)
-            vect1_non_pivot = np.array([arrtf[i] * (avg / len(e["texte"])) for i, e in enumerate(groupe)], dtype = np.half)
+            vect1_pivot = np.array([self.arrtf [i] * (avg / len(e)) for i, e in self.pivots], dtype = np.half)
+            vect1_non_pivot = np.array([self.arrtf [i] * (avg / len(e)) for i, e in enumerate(groupe)], dtype = np.half)
             array_vecteurs = pairwise_cos_dist(vect1_non_pivot, vect1_pivot)
             del vect1_non_pivot, vect1_pivot
 
@@ -56,7 +61,7 @@ class avance():
             for i in range(np.shape(matrice_cosine)[0]): 
                 matrice_cosine[i,i] = 1
             #Liste de toutes les distances faibles --> Doublons
-            lst_doublons = np.transpose(np.nonzero(matrice_cosine < 0.00001)).tolist()
+            lst_doublons = np.transpose(np.nonzero(matrice_cosine < sensibilite)).tolist()
 
             del matrice_cosine
 
@@ -68,12 +73,13 @@ class avance():
             del lst_doublons
 
             for db in doublons:
-                doublons_tot.add((taillegroupe * i) + db)
-                cquoilesdoublons.add(groupe.pop(db))
+                x = (groupe.pop(db))
+                liste_doublons.append(x[0])
+                index_doublons.append(x[1])
 
             del doublons
 
         self.liste_propre = list(chain.from_iterable(groupes))
-        self.index_doublons = doublons_tot
-        self.doublons = cquoilesdoublons
+        self.index_doublons = sorted(index_doublons)
+        self.liste_doublons = sorted(liste_doublons, key=lambda liste_doublons: len(liste_doublons))
         tqdm.write(f"Dédoublonage avancé : Il reste désormais {len(liste_txt)} articles.")
